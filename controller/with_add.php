27d7d6c20f44ee5cb44f8603392051f924aa603f
Mathieu <mathieu@MacBook-Pro-de-Mathieu.local>
2017-11-08 15:22:48 +0100

    add withdraw/adding

--- controller/with_add.php
+++ controller/with_add.php
@@ -2,12 +2,26 @@
 include 'model/connect.php';
 $manager = new BankManager($db);
 
+
+
 // withdraw/adding
 if(isset($_POST['sendadding']) || isset($_POST['sendwithdraw'])) {
-  $get=$manager->get($_POST['sendadding']);
+  $get=$manager->get($_POST['id']);
   $account = new Bank ($get);
-  $manager->adding($account);
+  
+  if(isset($_POST['adding'])){
+  $account->adding($_POST['adding']);
+  }
+
+  if(isset($_POST['withdraw'])){
+  $account->withdrawal($_POST['withdraw']);
+  }
+
   $manager->update($account);
+  $list = $manager->getList();
+  foreach ($list as $key => $value) {
+    $object[$key]= new Bank($value);
+  }
   include 'view/index.php';
 }
 
